7bbf309024a18d436ee6f82b0c45857b1d099ad2
Mika Urtela <mika.urtela@soulaimstudios.com>
2011-12-10 03:44:03 +0200

    Fixed mouse handling breaking up after a window resize.

--- src/fi/csc/microarray/client/visualisation/methods/gbrowserng/controller/EventHandler.java
+++ src/fi/csc/microarray/client/visualisation/methods/gbrowserng/controller/EventHandler.java
@@ -7,17 +7,20 @@ import com.jogamp.newt.event.MouseEvent;
 import com.jogamp.newt.event.NEWTEvent;
 import com.jogamp.newt.event.WindowEvent;
 import fi.csc.microarray.client.visualisation.methods.gbrowserng.interfaces.GenosideComponent;
+import fi.csc.microarray.client.visualisation.methods.gbrowserng.view.GenoWindow;
 
 public class EventHandler {
 
 	private BlockingQueue<NEWTEvent> eventQueue = null;
+    private final GenoWindow window;
 	private GenoEvent genoEvent = new GenoEvent(800, 600);
 
     private final GenosideComponent client;
 
-	public EventHandler(GenosideComponent client, BlockingQueue<NEWTEvent> eventQueue) {
+	public EventHandler(GenoWindow hostWindow, GenosideComponent client, BlockingQueue<NEWTEvent> eventQueue) {
 	    this.client = client;
         this.eventQueue = eventQueue;
+        this.window = hostWindow;
     }
 
 	public void toggleFullscreen() {
@@ -26,7 +29,7 @@ public class EventHandler {
 	public void handleEvents() throws InterruptedException {
 		for (;;) {
 			NEWTEvent event = this.eventQueue.take();
-			this.genoEvent.event = event;
+            genoEvent.event = event;
 
 			if (event instanceof KeyEvent) {
 				KeyEvent keyEvent = (KeyEvent) event;
@@ -40,6 +43,9 @@ public class EventHandler {
 			} else if(genoEvent.event instanceof MouseEvent)  {
                 client.handle((MouseEvent)(genoEvent.event), genoEvent.getMouseGLX(), genoEvent.getMouseGLY());
             } else if (event instanceof WindowEvent) {
+                if(event.getEventType() == WindowEvent.EVENT_WINDOW_RESIZED) {
+                    genoEvent.setScreenSize(window.window.getWidth(), window.window.getHeight());
+                }
 			}
 		}
 	}
